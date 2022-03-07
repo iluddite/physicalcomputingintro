c1e51856613d3f0c6487fadf6677660b105fa2a3
iluddite <ritchiejj@gmail.com>
2022-03-07 15:29:35 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+basic.show_number(input.compass_heading())
+basic.show_string("Hello World!")
+basic.show_icon(IconNames.HAPPY)
---
+++ main.ts
@@ -0,0 +1,3 @@
+basic.showNumber(input.compassHeading())
+basic.showString("Hello World!")
+basic.showIcon(IconNames.Happy)
